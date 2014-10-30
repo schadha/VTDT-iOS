de94d13a81dda4390300d77b5573aac96bc1f54b
Sanchit Chadha <schadha@vt.edu>
2014-10-30 18:53:26 -0400

    Revert 69ce641..17b3238
    This rolls back to commit 69ce64190ba185f1d0031a9e40834ea9660b5eb7.

--- VTDT/BarsViewController.swift
+++ VTDT/BarsViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class BarsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    var user: FBGraphUser!
 
-    @IBOutlet var barTable: UITableView!
-    var barDict = [NSDictionary]()
-    var currBar = NSDictionary()    
+    var refreshControl:UIRefreshControl!
+
+    @IBOutlet var newsFeedTable: UITableView!
+    @IBOutlet var barImage: UIImageView!
+    @IBOutlet var barName: UILabel!
+    
+    @IBOutlet var newsButton: UIButton!
+    @IBOutlet var specialsButton: UIButton!
+    
+    @IBOutlet var specialsLabel: UILabel!
+    @IBOutlet var newsLabel: UILabel!
+    var newsFeedItems = [NSDictionary]();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
         barImage.layer.cornerRadius = barImage.frame.size.width / 2;
         barImage.clipsToBounds = true;
         let width:CGFloat = 3.0
@@ -45,10 +51,6 @@ class BarsViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         self.navigationController?.navigationBarHidden = false
         
-=======
-        //navigation setup
-        self.navigationController?.navigationBarHidden = false
->>>>>>> ragan-3
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: self, action: "popToRoot:")
         self.navigationItem.leftBarButtonItem?.tintColor = UIColor.whiteColor()
         
@@ -57,20 +59,12 @@ class BarsViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.navigationController?.navigationBar.translucent = true
         self.navigationController?.navigationBar.backgroundColor = UIColor.clearColor()
         
-<<<<<<< HEAD
         fetchNewsFeed()
     }
     
     func popToRoot(sender:UIBarButtonItem){
         self.navigationController?.popViewControllerAnimated(true)
         self.navigationController?.navigationBarHidden = true
-=======
-        getBarData()
-        barTable.delegate = self;
-        barTable.dataSource = self;
-        barTable.layer.cornerRadius = 10;
-        // Do any additional setup after loading the view.
->>>>>>> ragan-3
     }
 
     override func didReceiveMemoryWarning() {
@@ -85,46 +79,50 @@ class BarsViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return barDict.count
+        return newsFeedItems.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell:UITableViewCell = barTable.dequeueReusableCellWithIdentifier("barCell", forIndexPath: indexPath) as UITableViewCell
-        var barItem = barDict[indexPath.row]
-        cell.textLabel.text = barItem["name"] as? String
-        //cell.imageView.image = barItem["logo"] as UIImage
-        cell.imageView.layer.cornerRadius = cell.imageView.frame.size.width / 2;
-        cell.imageView.clipsToBounds = true;
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        currBar = barDict[indexPath.row]
-        performSegueWithIdentifier("barPage", sender: self)
-    }
-    
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-       var barPage: BarProfileViewController = segue.destinationViewController  as BarProfileViewController
-        barPage.barInfo = currBar
-        barPage.user = self.user
-    }
-    
-    func popToRoot(sender: UIBarButtonItem) {
-        self.navigationController?.popViewControllerAnimated(true)
-        self.navigationController?.navigationBarHidden = true
-    }
-
-    
-    func getBarData() {
+        var customCell:NewsFeedTableViewCell = newsFeedTable.dequeueReusableCellWithIdentifier("newsFeedCell", forIndexPath: indexPath) as NewsFeedTableViewCell
         
-        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.bars")!
+        var newsFeedRow:NSDictionary = newsFeedItems[indexPath.row]
+        
+        
+        //name of user where userid --> newsFeedRow["user"]
+        //use the userID to do a fetch to the user db table for first name, last name, profile picture
+//        var userID = user
+//        customCell.userName.text = userID.first_name + " " + userID.last_name
+        
+        //        customCell.messageText.text = newsFeedRow["message"] as? String
+        customCell.messageText.text = "hello this is a very long message about what i was doing at sharkey's last night.  it must be less than 140 characters or else it wont post."
+        
+        //profile pic of user based on user id
+        
+//        customCell.userProfPic.profileID = userID.objectID
+//        customCell.userProfPic.layer.cornerRadius = customCell.userProfPic.frame.size.width / 2;
+//        customCell.userProfPic.clipsToBounds = true;
+        
+        var timeElement = newsFeedRow["timePosted"] as String
+        var timeArray:[String] = timeElement.componentsSeparatedByString("T")[1].componentsSeparatedByString(":")
+        
+        let intTime = timeArray[0].toInt()
+        if intTime > 12 {
+            let newTime:Int = intTime! - 12
+            customCell.barLocation.text = "at Sharkey's around \(newTime):\(timeArray[1]) pm"
+            
+        }
+        else {
+            customCell.barLocation.text = "at Sharkey's around \(timeArray[0]):\(timeArray[1]) am"
+        }
+        
+        return customCell
+    }
+    
+    func fetchNewsFeed () -> () {
+        print("calling restful function\n")
+        //create url for restful request
+        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.newsfeed")
         
         var request:NSURLRequest = NSURLRequest(URL: url)
         
@@ -148,24 +146,21 @@ class BarsViewController: UIViewController, UITableViewDelegate, UITableViewData
                         print ("error parsing json file \n")
                     }
                     else {
+                        var x = 0
                         for item in jsonResult {
                             
                             //need to make sure that we are only appending new items--
                             //would it be worth it??
                             var dict:NSDictionary = item as NSDictionary
-                            self.barDict.append(dict)
+                            self.newsFeedItems += [dict]
                         }
                         
                         //populate tableview here with newFeeditems that get set asynchroniously above ^^^
                         //will not populate until all news feed items have been fetched.
                         //tableview methods will be called initially (when screen is loaded) but since method
                         //is asynchronious, global newFeedItems array will still be empty
-<<<<<<< HEAD
 //                        print (self.newsFeedItems)
                         self.newsFeedTable.reloadData()
-=======
-                        self.barTable.reloadData()
->>>>>>> ragan-3
                         
                     }
                 }
@@ -180,5 +175,42 @@ class BarsViewController: UIViewController, UITableViewDelegate, UITableViewData
         })
         
     }
+    
+    func refreshInvoked() {
+        
+        refresh(viaPullToRefresh: true)
+    }
+    
+    func refresh(viaPullToRefresh: Bool = false) {
+        
+        //reset newsfeeditems
+        newsFeedItems = [NSDictionary]()
+        //reload news feed data
+        fetchNewsFeed()
+        
+        if (viaPullToRefresh) {
+            self.refreshControl?.endRefreshing()
+        }
+    }
+    @IBAction func newsClicked(sender: AnyObject) {
+        
+        newsLabel.hidden = false;
+        specialsLabel.hidden = true;
+    }
+    @IBAction func specialsClicked(sender: AnyObject) {
+        
+        newsLabel.hidden = true;
+        specialsLabel.hidden = false;
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
--- VTDT/InitialViewController.swift
+++ VTDT/InitialViewController.swift
@@ -23,7 +23,27 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpScreen()
+        profileName.text = user.first_name + " " + user.last_name
+        print(user.objectID)
+        profileImage.profileID = user.objectID
+        profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
+        profileImage.clipsToBounds = true;
+        
+        let width:CGFloat = 3.0
+        profileImage.layer.borderWidth = width
+        profileImage.layer.borderColor = UIColor.whiteColor().CGColor
+
+        newsTableview.delegate = self;
+        newsTableview.dataSource = self;
+        newsTableview.layer.cornerRadius = 10;
+        
+        //set up refresh controller
+        var tableViewController = UITableViewController()
+        tableViewController.tableView = newsTableview
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshInvoked", forControlEvents: UIControlEvents.ValueChanged)
+        tableViewController.refreshControl = self.refreshControl
         
         fetchNewsFeed()
     }
@@ -56,8 +76,8 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         var newsFeedRow:NSDictionary = newsFeedItems[indexPath.row]
         
         
-        // userid --> newsFeedRow["user"]
-        //use the userID to do a fetch to the user db table for first name, last name
+        //name of user where userid --> newsFeedRow["user"]
+        //use the userID to do a fetch to the user db table for first name, last name, profile picture
         var userID = user
         customCell.userName.text = userID.first_name + " " + userID.last_name
         
@@ -65,13 +85,12 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
 //        customCell.messageText.text = "hello this is a very long message about what i was doing at sharkey's last night.  it must be less than 140 characters or else it wont post."
         
         //profile pic of user based on user id
-        //newsFeedRow["user"]
+        
         customCell.userProfPic.profileID = userID.objectID
         customCell.userProfPic.layer.cornerRadius = customCell.userProfPic.frame.size.width / 2;
         customCell.userProfPic.clipsToBounds = true;
         
         var timeElement = newsFeedRow["timePosted"] as String
-<<<<<<< HEAD
         print(timeElement);
         var timeArray:[String] = timeElement.componentsSeparatedByString("T")[1].componentsSeparatedByString(":")
         
@@ -79,10 +98,11 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         if intTime > 12 {
             let newTime:Int = intTime! - 12
             customCell.barLocation.text = "at Sharkey's around \(newTime):\(timeArray[1]) pm"
-=======
-        customCell.barLocation.text = getPostTime(timeElement)
->>>>>>> ragan-3
 
+        }
+        else {
+            customCell.barLocation.text = "at Sharkey's around \(timeArray[0]):\(timeArray[1]) am"
+        }
         
         return customCell
     }
@@ -91,6 +111,8 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBAction func detailClicked(sender: AnyObject) {
 
         performSegueWithIdentifier("profileView", sender: self)
+        print("do something\n")
+
     }
     @IBAction func whatClicked(sender: AnyObject) {
         
@@ -119,18 +141,11 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
     }
     
-    // # RESTFUL CALLS ===========================================================
-    
     func fetchNewsFeed () -> () {
 
         //create url for restful request
-<<<<<<< HEAD
 //        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.newsfeed")
                 var url:NSURL = NSURL(string:"http://localhost:8080/VTDT/webresources/com.group2.vtdt.newsfeed")
-=======
-        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.newsfeed")!
-        
->>>>>>> ragan-3
         /*
         {
         "username": "10152362398270868",
@@ -147,13 +162,12 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
             
             
             //success!
-<<<<<<< HEAD
             if (data != nil && error == nil)
-=======
-            if (data?.length > 0 && error == nil)
->>>>>>> ragan-3
             {
                 
+                //do this on main application thread
+                dispatch_async(dispatch_get_main_queue()) {
+                    
                     //parse json into array
                     var jsonResult: NSArray = NSJSONSerialization.JSONObjectWithData(data,
                         options:NSJSONReadingOptions.MutableContainers, error: nil) as NSArray
@@ -163,21 +177,16 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
                         print ("error parsing json file \n")
                     }
                     else {
-                        
-                        //do this on main application thread
-                        dispatch_async(dispatch_get_main_queue()) {
+                        var x = 0
+                        for item in jsonResult {
                             
-                            var x = 0
-                            for item in jsonResult {
-                            
-                                if x < 25 {
-                                    var dict:NSDictionary = item as NSDictionary
-                                    self.newsFeedItems += [dict]
-                                }
-                                else {
-                                    break
-                                }
-                                x++
+                            if x < 25 {
+                            var dict:NSDictionary = item as NSDictionary
+                            self.newsFeedItems += [dict]
+                            }
+                            else {
+                                break
+                            }
                         }
                         
                         //populate tableview here with newFeeditems that get set asynchroniously above ^^^
@@ -185,9 +194,9 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
                         //tableview methods will be called initially (when screen is loaded) but since method
                         //is asynchronious, global newFeedItems array will still be empty
                         self.newsTableview.reloadData()
-                        }
                         
                     }
+                }
                 
             }
                 
@@ -197,53 +206,6 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
             }
             
         })
-        
-    }
-    
-    //get request to get user first and last name based on userid
-    
-    
-    //GET TIME FUNCTION
-    
-    func getPostTime(timeElement:String) -> String {
-        var timeArray:[String] = timeElement.componentsSeparatedByString("T")[1].componentsSeparatedByString(":")
-        
-        let intTime = timeArray[0].toInt()
-        if intTime > 12 {
-            let newTime:Int = intTime! - 12
-            return "at Sharkey's around \(newTime):\(timeArray[1]) pm"
-            
-        }
-        else {
-            return "at Sharkey's around \(timeArray[0]):\(timeArray[1]) am"
-        }
-    }
-    
-    func setUpScreen() {
-        
-        profileName.text = user.first_name + " " + user.last_name
-        profileImage.profileID = user.objectID
-        profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
-        profileImage.clipsToBounds = true;
-        
-        let width:CGFloat = 3.0
-        profileImage.layer.borderWidth = width
-        profileImage.layer.borderColor = UIColor.whiteColor().CGColor
-        
-        whatToDoButton.layer.cornerRadius = 10;
-        whoToDoButton.layer.cornerRadius = 10;
-        
-        newsTableview.delegate = self;
-        newsTableview.dataSource = self;
-        newsTableview.layer.cornerRadius = 10;
-        
-        //set up refresh controller
-        var tableViewController = UITableViewController()
-        tableViewController.tableView = newsTableview
-        
-        refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "refreshInvoked", forControlEvents: UIControlEvents.ValueChanged)
-        tableViewController.refreshControl = self.refreshControl
         
     }
     
@@ -258,8 +220,7 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
         else if segue.identifier == "whatView" {
             var barsPage: BarsViewController = segue.destinationViewController as BarsViewController
-            
-                barsPage.user = self.user;
+            //        profPage.user = self.user;
         }
         else if segue.identifier == "whoView" {
             var friendsPage: FriendsViewController = segue.destinationViewController as FriendsViewController
