579662dd9a19562a2ceea29a5e32a44d018c57ef
Sanchit Chadha <schadha@vt.edu>
2014-10-30 19:03:57 -0400

    Revert 1b64f30..106589b
    This rolls back to commit 1b64f30e7b8551c9388ff5a359d340e016b83f30.

--- VTDT/InitialViewController.swift
+++ VTDT/InitialViewController.swift
@@ -26,7 +26,27 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpScreen()
+        profileName.text = user.first_name + " " + user.last_name
+        print(user.objectID)
+        profileImage.profileID = user.objectID
+        profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
+        profileImage.clipsToBounds = true;
+        
+        let width:CGFloat = 3.0
+        profileImage.layer.borderWidth = width
+        profileImage.layer.borderColor = UIColor.whiteColor().CGColor
+
+        newsTableview.delegate = self;
+        newsTableview.dataSource = self;
+        newsTableview.layer.cornerRadius = 10;
+        
+        //set up refresh controller
+        var tableViewController = UITableViewController()
+        tableViewController.tableView = newsTableview
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshInvoked", forControlEvents: UIControlEvents.ValueChanged)
+        tableViewController.refreshControl = self.refreshControl
         
         fetchNewsFeed()
     }
@@ -59,8 +79,8 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         var newsFeedRow:NSDictionary = newsFeedItems[indexPath.row]
         
         
-        // userid --> newsFeedRow["user"]
-        //use the userID to do a fetch to the user db table for first name, last name
+        //name of user where userid --> newsFeedRow["user"]
+        //use the userID to do a fetch to the user db table for first name, last name, profile picture
         var userID = user
         customCell.userName.text = userID.first_name + " " + userID.last_name
         
@@ -68,14 +88,23 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         customCell.messageText.text = "hello this is a very long message about what i was doing at sharkey's last night.  it must be less than 140 characters or else it wont post."
         
         //profile pic of user based on user id
-        //newsFeedRow["user"]
+        
         customCell.userProfPic.profileID = userID.objectID
         customCell.userProfPic.layer.cornerRadius = customCell.userProfPic.frame.size.width / 2;
         customCell.userProfPic.clipsToBounds = true;
         
         var timeElement = newsFeedRow["timePosted"] as String
-        customCell.barLocation.text = getPostTime(timeElement)
+        var timeArray:[String] = timeElement.componentsSeparatedByString("T")[1].componentsSeparatedByString(":")
+        
+        let intTime = timeArray[0].toInt()
+        if intTime > 12 {
+            let newTime:Int = intTime! - 12
+            customCell.barLocation.text = "at Sharkey's around \(newTime):\(timeArray[1]) pm"
 
+        }
+        else {
+            customCell.barLocation.text = "at Sharkey's around \(timeArray[0]):\(timeArray[1]) am"
+        }
         
         return customCell
     }
@@ -84,6 +113,8 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBAction func detailClicked(sender: AnyObject) {
 
         performSegueWithIdentifier("profileView", sender: self)
+        print("do something\n")
+
     }
     @IBAction func whatClicked(sender: AnyObject) {
         
@@ -107,12 +138,10 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
     }
     
-    // # RESTFUL CALLS ===========================================================
-    
     func fetchNewsFeed () -> () {
 
         //create url for restful request
-        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.newsfeed")!
+        var url:NSURL = NSURL(string:"http://jupiter.cs.vt.edu/VTDT-1.0/webresources/com.group2.vtdt.newsfeed")
         
         /*
         {
@@ -130,9 +159,12 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
             
             
             //success!
-            if (data?.length > 0 && error == nil)
+            if (data.length > 0 && error == nil)
             {
                 
+                //do this on main application thread
+                dispatch_async(dispatch_get_main_queue()) {
+                    
                     //parse json into array
                     var jsonResult: NSArray = NSJSONSerialization.JSONObjectWithData(data,
                         options:NSJSONReadingOptions.MutableContainers, error: nil) as NSArray
@@ -142,21 +174,16 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
                         print ("error parsing json file \n")
                     }
                     else {
-                        
-                        //do this on main application thread
-                        dispatch_async(dispatch_get_main_queue()) {
+                        var x = 0
+                        for item in jsonResult {
                             
-                            var x = 0
-                            for item in jsonResult {
-                            
-                                if x < 25 {
-                                    var dict:NSDictionary = item as NSDictionary
-                                    self.newsFeedItems += [dict]
-                                }
-                                else {
-                                    break
-                                }
-                                x++
+                            if x < 25 {
+                            var dict:NSDictionary = item as NSDictionary
+                            self.newsFeedItems += [dict]
+                            }
+                            else {
+                                break
+                            }
                         }
                         
                         //populate tableview here with newFeeditems that get set asynchroniously above ^^^
@@ -164,9 +191,9 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
                         //tableview methods will be called initially (when screen is loaded) but since method
                         //is asynchronious, global newFeedItems array will still be empty
                         self.newsTableview.reloadData()
-                        }
                         
                     }
+                }
                 
             }
                 
@@ -176,53 +203,6 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
             }
             
         })
-        
-    }
-    
-    //get request to get user first and last name based on userid
-    
-    
-    //GET TIME FUNCTION
-    
-    func getPostTime(timeElement:String) -> String {
-        var timeArray:[String] = timeElement.componentsSeparatedByString("T")[1].componentsSeparatedByString(":")
-        
-        let intTime = timeArray[0].toInt()
-        if intTime > 12 {
-            let newTime:Int = intTime! - 12
-            return "at Sharkey's around \(newTime):\(timeArray[1]) pm"
-            
-        }
-        else {
-            return "at Sharkey's around \(timeArray[0]):\(timeArray[1]) am"
-        }
-    }
-    
-    func setUpScreen() {
-        
-        profileName.text = user.first_name + " " + user.last_name
-        profileImage.profileID = user.objectID
-        profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
-        profileImage.clipsToBounds = true;
-        
-        let width:CGFloat = 3.0
-        profileImage.layer.borderWidth = width
-        profileImage.layer.borderColor = UIColor.whiteColor().CGColor
-        
-        whatToDoButton.layer.cornerRadius = 10;
-        whoToDoButton.layer.cornerRadius = 10;
-        
-        newsTableview.delegate = self;
-        newsTableview.dataSource = self;
-        newsTableview.layer.cornerRadius = 10;
-        
-        //set up refresh controller
-        var tableViewController = UITableViewController()
-        tableViewController.tableView = newsTableview
-        
-        refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "refreshInvoked", forControlEvents: UIControlEvents.ValueChanged)
-        tableViewController.refreshControl = self.refreshControl
         
     }
     
@@ -237,8 +217,7 @@ class InitialViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
         else if segue.identifier == "whatView" {
             var barsPage: BarsViewController = segue.destinationViewController as BarsViewController
-            
-                barsPage.user = self.user;
+            //        profPage.user = self.user;
         }
         
 
